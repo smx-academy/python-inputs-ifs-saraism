a7febdde24d8f05efb85d9c6608405b1148b0cd3
saraism <114435588+saraism@users.noreply.github.com>
2022-11-02 16:13:55 +0100

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,48 @@
+from tkinter import Y
+# x=int(input('Vnesete dvocifren broj\n'))
+# y=int(input('Vnesete dvocifren broj\n'))
+# zbir=x+y
+# if zbir < 100:
+#     print('Zbirot na vnesenite borevi e {} i e pomal od 100'.format(zbir))
+# else:
+#     print('Zbirot na vnesenite borevi e {} i e pogolem od 100'.format(zbir))
+
+# godini=int(input('Vnesete ja vasata godina na ragjanje\n'))
+# if godini > 2004:
+#     print('Vie ste maloleten')
+# else:
+#     print('Vie ste polnoleten')
+
+# print('Vnesete gi stranite na pravoagolnikot A')
+# a=int(input('Vnesete broj\n'))
+# b=int(input('Vnesete broj\n'))
+# print('Vnesete gi stranite na pravoagolnikot B')
+# x=int(input('Vnesete broj\n'))
+# y=int(input('Vnesete broj\n'))
+# plostinaA=a*b
+# plostinaB=x*y
+
+# if plostinaA>plostinaB:
+#     print('Pogolema plostina ima pravoagolnikot A, koja sto iznesuva {}'.format(plostinaA))
+# else:
+#     print('Pogolema plostina ima pravoagolnikot B koja sto iznesuva {}'.format(plostinaB))
+
+# d=int(input('Vnesete go agolot an stranata a od triagolnikot\n'))
+# b=int(input('Vnesete go agolot an stranata b od triagolnikot\n'))
+# y=int(input('Vnesete go agolot an stranata c od triagolnikot\n'))
+# zbir=d+b+y
+
+# if zbir==180:
+#     print('Triagolnikot moze da se formira bidejki zbirot na vnesenite agli e 180o')
+# else:
+#     print('Triagolnikot ne moze da se formira bidejki zbirot na vnesenite agli ne e 180o')
+
+# ime=input('Vnesete go vaseto ime\n')
+# karakteri=(len(ime))
+# a=ime.count('a')
+# e=ime.count('e')
+# i=ime.count('i')
+# o=ime.count('o')
+# u=ime.count('u')
+# samoglaski=a+e+i+o+u
+# print('Vaseto ime ima {} karakteri i e sostaveno od {} samoglaski i toa A={},E={},I={},O={},U={}'.format(karakteri,samoglaski,a,e,i,o,u))
